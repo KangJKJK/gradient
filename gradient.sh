99ec068cef3c0cb2ac462a1400f6eb6f3dd50022
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-11-09 14:57:53 +0900

    Add files via upload

---
+++ gradient.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# 색깔 변수 정의
+BOLD='\033[1m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+# 환경 변수 설정
+export WORK="/root/gradient-bot"
+
+# 사용자 선택 메뉴
+echo -e "${GREEN}스크립트작성자: https://t.me/kjkresearch${NC}"
+echo -e "${BOLD}${CYAN}1. gradient 노드 설치${NC}"
+echo -e "${BOLD}${CYAN}2. gradient 노드 업데이트${NC}"
+read -p "원하는 작업을 선택하세요 (1/2): " choice
+
+case $choice in
+    1)
+        echo -e "${YELLOW}gradient 노드를 설치합니다.${NC}"
+
+        # 필수 패키지 설치
+        echo -e "${BOLD}${CYAN}필수 패키지 설치 중...${NC}"
+        sudo apt-get update
+        sudo apt-get -y upgrade
+        sudo apt-get install -y ufw
+
+        echo -e "${YELLOW}작업 공간 준비 중...${NC}"
+        if [ -d "$WORK" ]; then
+            echo -e "${YELLOW}기존 작업 공간 삭제 중...${NC}"
+            rm -rf "$WORK"
+        fi
+
+        git clone https://github.com/airdropinsiders/HumanityTestnet-Bot.git
+        cd "$WORK"
+
+        # Docker GPG 키 추가
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+        # Docker 저장소 추가
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+        # Docker 설치
+        sudo apt-get update
+        sudo apt-get install -y docker-ce
+
+        # Docker Compose 설치
+        sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+
+        # Docker 서비스 시작
+        sudo systemctl start docker
+
+        # Docker 서비스가 부팅 시 자동으로 시작되도록 설정
+        sudo systemctl enable docker
+
+        # 프록시 정보 입력 안내
+        echo -e "${YELLOW}프록시 정보를 입력하세요. 입력형식: http://user:pass@ip:port${NC}"
+        echo -e "${YELLOW}여러 개의 프록시는 줄바꿈으로 구분하세요.${NC}"
+        echo -e "${YELLOW}입력을 마치려면 엔터를 두 번 누르세요.${NC}"
+
+        # 프록시 정보를 직접 proxy.txt 파일에 저장
+        > "$WORK/proxy.txt"  # 파일 초기화
+        while IFS= read -r line; do
+            [[ -z "$line" ]] && break
+            # http 형식을 socks5 형식으로 변환
+            socks5_line="${line/http:/socks5:}"
+            echo "$socks5_line" >> "$WORK/proxies.txt"
+        done
+
+        echo -e "${GREEN}프록시 정보가 proxy.txt 파일에 저장되었습니다.${NC}"
+
+        # 사용자에게 이메일과 비밀번호 입력 받기
+        read -p "이메일을 입력하세요: " APP_USER
+        read -p "비밀번호를 입력하세요: " APP_PASS
+        echo
+
+        # Docker 명령어 실행
+        docker run -d \
+        -e APP_USER="$APP_USER" \
+        -e APP_PASS="$APP_PASS" \
+        -v ./proxies.txt:/app/proxies.txt \
+        overtrue/gradient-bot
+        ;;
+    2)
+        echo -e "${YELLOW}gradient 노드를 업데이트합니다.${NC}"
+        cd "$WORK"
+        sudo apt-get update
+        sudo apt-get -y upgrade
+        # gradient 컨테이너 제거
+        echo -e "${YELLOW}현재 실행 중인 gradient 관련 컨테이너 목록:${NC}"
+        docker ps | grep gradient  # gradient 관련 컨테이너 목록 출력
+        read -p "제거할 gradient 컨테이너 ID를 입력하세요: " container_id
+        docker rm -f "$container_id"  # 컨테이너 강제 제거
+        echo -e "${GREEN}컨테이너 $container_id 가 제거되었습니다.${NC}"
+        docker pull overtrue/gradient-bot
+
+         # 사용자에게 이메일과 비밀번호 입력 받기
+        read -p "이메일을 입력하세요: " APP_USER
+        read -p "비밀번호를 입력하세요: " APP_PASS
+        echo
+
+        # Docker 명령어 실행
+        docker run -d \
+        -e APP_USER="$APP_USER" \
+        -e APP_PASS="$APP_PASS" \
+        -v ./proxies.txt:/app/proxies.txt \
+        overtrue/gradient-bot
+        ;;
